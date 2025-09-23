2b901cedb44d81fa5dbcdc17ad7cd81a98ba2a2b
dean131 <deanabdillah@gmail.com>
2025-09-23 11:16:30 +0700

    fix: remove stand alone storage service

--- apps/storage-service/src/api/files/files.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generatePresignedUrl } from "./files.controller.js";
-
-const router = Router();
-
-router.post("/upload-url", generatePresignedUrl);
-
-export default router;
--- apps/storage-service/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import { config } from "./config/index.js";
-
-const PORT = config.port;
-
-app.listen(PORT, () => {
-  console.log(`Storage Service is running on http://localhost:${PORT}`);
-});
